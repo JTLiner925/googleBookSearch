2efaa9fbe42fe4837f67012d686f06fb6c6457f4
JTLiner925 <[jtliner925@gmail.com]>
2020-04-11 17:51:13 -0500

    started workin gon app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,42 @@ import BookResults from "./BookResults/BookResults";
 import BookSearch from "./BookSearch/BookSearch";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      q: [],
+      printType: [],
+      filter: [],
+      selected: null
+    };
+  }
+  componentDidMount() {
+    fetch("https://www.googleapis.com/auth/books")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Something went wrong, please try again later.");
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then(data => {
+        const countries = Object.keys(data).map(key => data[key].item[0]);
+        this.setState({
+          countries,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  }
+  setSelected(selected) {
+    this.setState({
+      selected
+    });
+  }
   render() {
     
     return (
